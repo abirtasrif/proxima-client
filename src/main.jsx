906891a95fa0dc6a490dceb2fec7fc13dc8a558d
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-02-22 19:40:23 +0600

    adding context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ProjectContextProvider } from "./context/ProjectContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProjectContextProvider>
+        <App />
+      </ProjectContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
