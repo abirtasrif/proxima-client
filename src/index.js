66ccfe84646039d7fdea896326cab9861fbd0cc7
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-02-24 00:47:18 +0600

    migrating to cra from vite

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ProjectContextProvider } from "./context/ProjectContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProjectContextProvider>
